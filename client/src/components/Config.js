2994b6bbea569ec446a476f4cfd2050e7bc051af
Kristijan Maoduš <kmaodus@foi.hr>
2020-08-05 17:59:10 +0200

    Landing page done

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,17 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'b4a549abb798b19dbb7e63335d135053';
+
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,106 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
-
+import React, { useEffect, useState, useRef } from 'react'
+import { Typography, Row, Button } from 'antd';
+import { API_URL, API_KEY, IMAGE_BASE_URL, IMAGE_SIZE, POSTER_SIZE } from '../../Config'
+import MainImage from './Sections/MainImage'
+import GridCard from '../../commons/GridCards'
+const { Title } = Typography;
 function LandingPage() {
+    const buttonRef = useRef(null);
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [Loading, setLoading] = useState(true)
+    const [CurrentPage, setCurrentPage] = useState(0)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetchMovies(endpoint)
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, [])
+
+
+    const fetchMovies = (endpoint) => {
+
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                // console.log(result)
+                // console.log('Movies',...Movies)
+                // console.log('result',...result.results)
+                setMovies([...Movies, ...result.results])
+                setMainMovieImage(MainMovieImage || result.results[0])
+                setCurrentPage(result.page)
+            }, setLoading(false))
+            .catch(error => console.error('Error:', error)
+            )
+    }
+
+    const loadMoreItems = () => {
+        let endpoint = '';
+        setLoading(true)
+        console.log('CurrentPage', CurrentPage)
+        endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint);
+
+    }
+
+    const handleScroll = () => {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        const windowBottom = windowHeight + window.pageYOffset;
+        if (windowBottom >= docHeight - 1) {
+
+            // loadMoreItems()
+            console.log('clicked')
+            buttonRef.current.click();
+
+        }
+    }
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+            {MainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}${IMAGE_SIZE}${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+
+            }
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <Title level={2} > Movies by latest </Title>
+                <hr />
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCard
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                                    : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+
+                {Loading &&
+                    <div>Loading...</div>}
+
+                <br />
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button ref={buttonRef} className="loadMore" onClick={loadMoreItems}>Load More</button>
+                </div>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using</div>
-        </>
+
+        </div>
     )
 }
 
