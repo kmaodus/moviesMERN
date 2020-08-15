05403ddacea8baef09582cb02ac3008af99c51c8
Kristijan Maoduš <kmaodus@foi.hr>
2020-08-16 00:04:29 +0200

    Movie description reordered

--- client/src/components/views/MovieDetail/Sections/MovieInfo.js
+++ client/src/components/views/MovieDetail/Sections/MovieInfo.js
@@ -9,12 +9,13 @@ function MovieInfo(props) {
     <Descriptions title="Movie details" bordered size='default'>
       <Descriptions.Item style={{ padding: '400px' }} label="Title">{movie.original_title}</Descriptions.Item>
       <Descriptions.Item label="Release date">{movie.release_date}</Descriptions.Item>
-      <Descriptions.Item label="Revenue">{movie.revenue}</Descriptions.Item>
-      <Descriptions.Item label="Runtime">{movie.runtime}</Descriptions.Item>
-      <Descriptions.Item label="Average vote" span={2}>{movie.vote_average}</Descriptions.Item>
+      <Descriptions.Item label="Revenue">{movie.revenue} USD</Descriptions.Item>
+      <Descriptions.Item label="Runtime">{movie.runtime} minutes</Descriptions.Item>
       <Descriptions.Item label="Vote count">{movie.vote_count}</Descriptions.Item>
-      <Descriptions.Item label="Status">{movie.status}</Descriptions.Item>
+      {/* <Descriptions.Item label="Status">{movie.status}</Descriptions.Item> */}
+      <Descriptions.Item label="Homepage">{movie.homepage}</Descriptions.Item>
       <Descriptions.Item label="Popularity">{movie.popularity}</Descriptions.Item>
+      <Descriptions.Item label="Average vote" span={2}>{movie.vote_average}</Descriptions.Item>
     </Descriptions>
   )
 }
