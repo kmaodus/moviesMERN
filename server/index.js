c272fa3a87fbc894fe5583111c3d425150530d57
Kristijan Maoduš <kmaodus@foi.hr>
2020-08-15 18:02:15 +0200

    Setup proxy fixed

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
-app.use('/api/comment', require('./routes/comment'));
-app.use('/api/like', require('./routes/like'));
-app.use('/api/favorite', require('./routes/favorite'));
+// app.use('/api/comment', require('./routes/comment'));
+// app.use('/api/like', require('./routes/like'));
+// app.use('/api/favorite', require('./routes/favorite'));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
