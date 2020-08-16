d1a7649b4e831c7d7b7b461329e4f94d617769fb
Kristijan Maoduš <kmaodus@foi.hr>
2020-08-16 02:54:26 +0200

    Comments implemented

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
-// app.use('/api/comment', require('./routes/comment'));
+app.use('/api/comment', require('./routes/comment'));
 app.use('/api/like', require('./routes/like'));
 app.use('/api/favorite', require('./routes/favorite'));
 
@@ -37,5 +37,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  console.log(`Server Running at port ${port}`)
 });
